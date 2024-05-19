e1731a9bd4085c0b981c370a6fb15b94f8cfcc79
devfxx <fxx@kty.lol>
2024-05-19 12:47:45 +0000

    feat: Add /uwu endpoint back

--- src/server_uwu.ts
+++ src/server_uwu.ts
@@ -4,7 +4,17 @@ import { serve } from "bun";
 
 const app = new Hono();
 
-app.get("/:text", (c) => c.text(uwuify(c.req.param("text") || "")));
+app.get("/", (c) =>
+  c.json({
+    instructions:
+      "Use /uwu endpoint with 'message' query parameter to UwUify your text.",
+  })
+);
+
+app.get("/uwu", (c) => {
+  const message = c.req.query("message") || "";
+  return c.text(uwuify(message));
+});
 
 serve({
   fetch: app.fetch,
